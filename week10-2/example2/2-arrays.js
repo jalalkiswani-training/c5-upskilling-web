ba833ea9061d0cbe6189b36e158d614558b84adb
Jalal Kiswani <kiswanij@gmail.com>
2020-09-07 12:25:28 +0300

    .

--- week10-2/example2/2-arrays.js
+++ week10-2/example2/2-arrays.js
@@ -3,13 +3,13 @@ var x2 = 20;
 
 var arr = ["Ata", "Jamal", "Essa"];
 console.log(arr);
-/*
+
 var f1 = function(item, index) {
     console.log(`Item at ${index} = ${item} `)
 };
 
 arr.forEach(f1);
-*/
+
 
 /*
 arr.forEach(function(item, index) {
